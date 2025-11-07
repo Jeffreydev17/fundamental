81cdd7aae8dbf13b3d55a0cfbe529dfcf165f220
Jeffreydev17 <ngokachinemerem3@gmail.com>
2025-11-07 04:47:52 -0500

    this is javascript file1

---
+++ index.js
@@ -0,0 +1,8 @@
+/** 
+ * create a scripts to print "javascripts is fun" to the console
+ * you are not allowed to use var
+ * you are only to scripts in one line of code
+ */ 
+
+
+console.log("javascripts is fun")
