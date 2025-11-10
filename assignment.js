ff2de3a2992efdc7db415db0383cf70df4849659
Jeffreydev17 <ngokachinemerem3@gmail.com>
2025-11-10 18:21:42 -0500

    task 3 of the assign

--- assignment.js
+++ assignment.js
@@ -2,16 +2,16 @@
 //  * Create a function that reverses a string without using built-in reverse() method
 //  */
 
-function reverseString(str) {
-    let reversed = "";
-    for (let i = str.length - 1; i >= 0; i--)
-        {
-        reversed += str[i];
-    }
-    return reversed;
-}
-    console.log(reverseString("hello"));
-    console.log(reverseString("happiness"))
+// function reverseString(str) {
+//     let reversed = "";
+//     for (let i = str.length - 1; i >= 0; i--)
+//         {
+//         reversed += str[i];
+//     }
+//     return reversed;
+// }
+//     console.log(reverseString("hello"));
+//     console.log(reverseString("happiness"))
 // Test cases
 // console.log(reverseString("hello"));    // "olleh"
 // console.log(reverseString("JavaScript")); // "tpircSavaJ"
@@ -24,24 +24,49 @@ function reverseString(str) {
 //  * - For multiples of 5, print "Buzz" instead of the number
 //  * - For multiples of both 3 and 5, print "FizzBuzz"
 //  */
-function fizzBuzz(n) {
-    for (let A = 1; A <= n; A++) {
-        if (A % 3 === 0 && A % 5 === 0) {
-            console.log("FizzBuzz");
-        }  else if (A % 3 === 0) {
-    console.log("Fizz")
-}
-    else if (A % 5 === 0) {console.log("Buzz")
+// function fizzBuzz(n) {
+//     for (let A = 1; A <= n; A++) {
+//         if (A % 3 === 0 && A % 5 === 0) {
+//             console.log("FizzBuzz");
+//         }  else if (A % 3 === 0) {
+//     console.log("Fizz")
+// }
+//     else if (A % 5 === 0) {console.log("Buzz")
 
-    } else {
-        console.log(A);
-    }
-    } 
-    // Your code here
-}
-fizzBuzz(15);
+//     } else {
+//         console.log(A);
+//     }
+//     } 
+//     // Your code here
+// }
+// fizzBuzz(15);
 
 // Test case
 // fizzBuzz(15);
 // Should output: 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz
+
+
+
+
+// * Write a function that takes a sentence and returns the longest word
+//  * If there are multiple words with the same length, return the first one
+//  */
+function findLongestWord(sentence) {
+    const words = sentence.split (" ");
+    let longest = "";
+
+    for (let word of words) {
+        if (word.length > longest.length) {
+            longest = word;
+        }
+    }
+    return longest;
+    // Your code here
+}
+console.log(findLongestWord("The quick brown fox jumped over the lazy dog"))
+console.log(findLongestWord("Hello world programming"))
+
+// Test cases
+// console.log(findLongestWord("The quick brown fox jumped over the lazy dog")); // "jumped"
+// console.log(findLongestWord("Hello world programming")); // "programming"
 
--- calculator.js
+++ calculator.js
@@ -39,15 +39,61 @@
 // 60-69: "D"
 // Below 60: "F"
 
-function calculateGrade(score){
-    if (score >= 90 && score <= 100) {
-        return "A"
-    } else if (score >= 80 &&  score <=89) {
-        return "B"
-    } else if (score >= 70 && score <= 79) {
-        return "C"
-    }
+// function calculateGrade(score){
+//     if (score >= 90 && score <= 100) {
+//         return "A"
+//     } else if (score >= 80 &&  score <=89) {
+//         return "B"
+//     } else if (score >= 70 && score <= 79) {
+//         return "C"
+//     }
+// }
+// console.log(calculateGrade(95))
+// console.log(calculateGrade(82))
+// console.log(calculateGrade(70))
+
+
+/**
+ * Create a function that reverses a string without using built-in reverse() method
+ */
+// function reverseString(str) {
+//     let reversed = "";
+//     for (let i = str.length - 1; i >= 0; i--)
+//         {
+//         reversed += str[i];
+//     }
+//     return reversed;
+// }
+//     console.log(reverseString("hello"));
+//     console.log(reverseString("happiness"))
+// // Test cases
+// console.log(reverseString("hello"));    // "olleh"
+// console.log(reverseString("JavaScript")); // "tpircSavaJ"
+
+
+// Write a function that prints numbers from 1 to n, but:
+//  * - For multiples of 3, print "Fizz" instead of the number
+//  * - For multiples of 5, print "Buzz" instead of the number
+//  * - For multiples of both 3 and 5, print "FizzBuzz"
+//  */
+function fizzBuzz(n) {
+    for (let A = 1; A <= n; A++) {
+        if (A % 3 === 0 && A % 5 === 0) {
+            console.log("FizzBuzz");
+        }  else if (A % 3 === 0) {
+    console.log("Fizz")
 }
-console.log(calculateGrade(95))
-console.log(calculateGrade(82))
-console.log(calculateGrade(70))
+    else if (A % 5 === 0) {console.log("Buzz")
+
+    } else {
+        console.log(A);
+    }
+    } 
+    // Your code here
+}
+fizzBuzz(15);
+
+// Test case
+// fizzBuzz(15);
+// Should output: 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz
+
