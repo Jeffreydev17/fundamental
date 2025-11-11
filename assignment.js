90d82f179bc59b8131dd9f22295ab2b7c9a58a05
Jeffreydev17 <ngokachinemerem3@gmail.com>
2025-11-11 15:15:22 -0500

    Task 5 OF THE ASSIGNMENT

--- assignment.js
+++ assignment.js
@@ -51,22 +51,40 @@
 // * Write a function that takes a sentence and returns the longest word
 //  * If there are multiple words with the same length, return the first one
 //  */
-function findLongestWord(sentence) {
-    const words = sentence.split (" ");
-    let longest = "";
+// function findLongestWord(sentence) {
+//     const words = sentence.split (" ");
+//     let longest = "";
 
-    for (let word of words) {
-        if (word.length > longest.length) {
-            longest = word;
-        }
-    }
-    return longest;
-    // Your code here
-}
-console.log(findLongestWord("The quick brown fox jumped over the lazy dog"))
-console.log(findLongestWord("Hello world programming"))
+//     for (let word of words) {
+//         if (word.length > longest.length) {
+//             longest = word;
+//         }
+//     }
+//     return longest;
+//     // Your code here
+// }
+// console.log(findLongestWord("The quick brown fox jumped over the lazy dog"))
+// console.log(findLongestWord("Hello world programming"))
 
 // Test cases
 // console.log(findLongestWord("The quick brown fox jumped over the lazy dog")); // "jumped"
 // console.log(findLongestWord("Hello world programming")); // "programming"
+
+
+
+//  Write a function that takes an array of numbers and returns a new array with:
+//  * - All even numbers doubled
+//  * - All odd numbers tripled
+//  * - Remove any numbers greater than 100 from the final result
+//  */
+function manipulateArray(numbers) {
+    return numbers
+        .map(num =>(num % 2 === 0 ? num * 2 : num * 3))
+        .filter(num => num <= 100);
+    // Your code here
+}
+
+
+console.log(manipulateArray([1, 2, 3, 4, 5, 60, 40])); 
+// Should return [3, 4, 9, 8, 15] (60*2=120 which is >100, so removed)// Test case
 
